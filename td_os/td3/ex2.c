556e0d880a74ca95dcac9056178e43bf7f28ccb5
pepnou <pepnou1@gmail.com>
2017-03-22 10:54:03 +0100

    oui

--- td_os/td3/ex2.c
+++ td_os/td3/ex2.c
@@ -14,7 +14,15 @@ typedef struct
 
 void* max(void* arg)
 {
+	donnees* a = (donnees*)arg;
+	int i;
 	
+	for(i = a -> deb;i < a -> fin;i++)
+	{
+		if(a->tab[i] > a->max)a->max = a->tab[i];
+	}
+	
+	return NULL;
 }
 
 int main()
@@ -23,7 +31,7 @@ int main()
 	
 	pthread_t tid[nbr_thread];
 	donnees info[nbr_thread+1];
-	int alea[nbr_case],i;
+	int alea[nbr_case],i,res = 0;
 	
 	for(i=0;i<nbr_case;i++)alea[i]=rand()%1000 + 1;
 	
@@ -36,6 +44,15 @@ int main()
 		
 		pthread_create(&tid[i],NULL,max,&info[i]);
 	}
+	
+	info[i].deb = nbr_case - nbr_case % nbr_thread;
+	info[i].fin = nbr_case - 1;
+	info[i].max = 0;
+	info[i].tab = alea;
+	
+	max(&info[i]);
+	
+	for(i=0;i<nbr_thread;i++) pthread_join(tid[i],NULL);
 	
 	return 0;
 }
