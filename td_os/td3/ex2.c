19295a88ce2e6f5a95ef1beac6e65a2fa2849b3f
pepnou <pepnou1@gmail.com>
2017-03-22 10:37:51 +0100

    oui

--- td_os/td3/ex2.c
+++ td_os/td3/ex2.c
@@ -3,14 +3,24 @@
 #include <time.h>
 
 #define nbr_case 20
+#define nbr_thread 15
+typedef struct
+{
+	int deb,fin,max;
+}donnees;
+
+void* max(
 
 int main()
 {
 	srand(time(NULL));
 	
+	pthread_t tid[nbr_thread];
+	donnees info[nbr_thread];
 	int alea[nbr_case],i;
 	
 	for(i=0;i<nbr_case;i++)alea[i]=rand()%1000 + 1;
+	
 	
 	
 	return 0;
