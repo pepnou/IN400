3c054da264e6d602dfc3109b5851e5d6f5ab237d
pepnou <pepnou1@gmail.com>
2017-03-22 21:01:09 +0100

    oui

--- yolo_test/dezoom/dezoom.c
+++ yolo_test/dezoom/dezoom.c
@@ -75,29 +75,6 @@ Uint32 obtenirPixel(SDL_Surface *surface, int x, int y)
     }
 }
 
-/*SDL_Surface *ScaleSurface(SDL_Surface *Surface, Uint16 Width, Uint16 Height)
-{
-    if(!Surface || !Width || !Height)
-        return 0;
-     
-    SDL_Surface *_ret = SDL_CreateRGBSurface(Surface->flags, Width, Height, Surface->format->BitsPerPixel,
-        Surface->format->Rmask, Surface->format->Gmask, Surface->format->Bmask, Surface->format->Amask);
-
-	double _stretch_factor_x = (double)Width  / (double)Surface->w;
-    double _stretch_factor_y = (double)Height / (double)Surface->h;
-	
-	int y,x,o_y,o_x;
-	
-    for(y = 0; y < Surface->h; y++)
-        for(x = 0; x < Surface->w; x++)
-            for(o_y = 0; o_y < _stretch_factor_y; ++o_y)
-                for(o_x = 0; o_x < _stretch_factor_x; ++o_x)
-					definirPixel(_ret,((Sint32)_stretch_factor_x * x) + o_x, ((Sint32)_stretch_factor_y * y) + o_y,obtenirPixel(Surface,x,y));
-                    //DrawPixel(_ret, ((Sint32)_stretch_factor_x * x) + o_x, ((Sint32)_stretch_factor_y * y) + o_y, ReadPixel(Surface, x, y));
- 
-    return _ret;
-}*/
-
 Uint32 moy_zone(SDL_Surface *Surface,int x,int y,int largeur,int hauteur)
 {
 	Uint32 pixel,pixel_tmp;
@@ -140,9 +117,9 @@ SDL_Surface* dezoom_v1(SDL_Surface *Surface,int z_lvl)
 	
 	int i,j,largeur,hauteur;
 	
-	for(i=0;i<(Surface->w/z_lvl)+(Surface->w%z_lvl != 0);i++)
+	for(i=0;i<Width;i++)
 	{
-		for(j=0;j<(Surface->h/z_lvl)+(Surface->h%z_lvl != 0);j++)
+		for(j=0;j<Height;j++)
 		{
 			definirPixel(_ret,i,j,moy_zone(Surface,i*z_lvl,j*z_lvl,z_lvl,z_lvl));
 		}
@@ -151,37 +128,138 @@ SDL_Surface* dezoom_v1(SDL_Surface *Surface,int z_lvl)
 	return _ret;
 }
 
-void test()
+void test_v1()
 {
 	SDL_Init(SDL_INIT_VIDEO);
 	
-	SDL_Surface* image = SDL_LoadBMP("plan_metro_paris_2017.bmp");
-	SDL_Surface* image_z = dezoom_v1(image,3);
-	SDL_Surface* ecran = SDL_SetVideoMode(image_z->w,image_z->h,32,SDL_HWSURFACE);
+	SDL_Surface* image = SDL_LoadBMP("plan-metro-paris-2017.bmp");
+	SDL_Surface* image_z;
+	SDL_Surface* ecran;
 	
 	SDL_Rect position;position.x=0;position.y=0;
 	
-	//SDL_BlitSurface(image,NULL,ecran,&position);
-	SDL_BlitSurface(image_z,NULL,ecran,&position);
+	int i;
 	
-	SDL_Flip(ecran);
-	
-	pause();
+	for(i=1;i<10;i++)
+	{
+		image_z = dezoom_v1(image,i);
+		ecran = SDL_SetVideoMode(image_z->w,image_z->h,32,SDL_HWSURFACE);
+		
+		SDL_BlitSurface(image_z,NULL,ecran,&position);
+		
+		SDL_Flip(ecran);
+		
+		pause();
+	}
 	
 	SDL_FreeSurface(image);
 	SDL_FreeSurface(ecran);
 	SDL_Quit();
 }
 
+SDL_Surface* dezoom_v2(SDL_Surface *Surface,int z_lvl)
+{
+	int Width = (Surface->w/(z_lvl-1))+(Surface->w%(z_lvl-1) != 0);
+	int Height = (Surface->h/(z_lvl-1))+(Surface->h%(z_lvl-1) != 0);
+	
+	SDL_Surface *_ret = SDL_CreateRGBSurface(Surface->flags, Width, Height, Surface->format->BitsPerPixel,Surface->format->Rmask, Surface->format->Gmask, Surface->format->Bmask, Surface->format->Amask);
+	
+	int i,j,largeur,hauteur;
+	
+	for(i=0;i<Width;i++)
+	{
+		for(j=0;j<Height;j++)
+		{
+			definirPixel(_ret,i,j,moy_zone(Surface,i*(z_lvl-1),j*(z_lvl-1),z_lvl,z_lvl));
+		}
+	}
+	
+	return _ret;
+}
 
+void test_v2()
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	
+	SDL_Surface* image = SDL_LoadBMP("plan-metro-paris-2017.bmp");
+	SDL_Surface* image_z;
+	SDL_Surface* ecran;
+	
+	SDL_Rect position;position.x=0;position.y=0;
+	
+	int i;
+	
+	for(i=2;i<10;i++)
+	{
+		image_z = dezoom_v2(image,i);
+		ecran = SDL_SetVideoMode(image_z->w,image_z->h,32,SDL_HWSURFACE);
+		
+		SDL_BlitSurface(image_z,NULL,ecran,&position);
+		
+		SDL_Flip(ecran);
+		
+		pause();
+	}
+	
+	SDL_FreeSurface(image);
+	SDL_FreeSurface(ecran);
+	SDL_Quit();
+}
 
+SDL_Surface* dezoom_vf(SDL_Surface *Surface,int z_lvl,int recouvrement)
+{
+	int Width = (Surface->w/(z_lvl-recouvrement))+(Surface->w%(z_lvl-recouvrement) != 0);
+	int Height = (Surface->h/(z_lvl-recouvrement))+(Surface->h%(z_lvl-recouvrement) != 0);
+	
+	SDL_Surface *_ret = SDL_CreateRGBSurface(Surface->flags, Width, Height, Surface->format->BitsPerPixel,Surface->format->Rmask, Surface->format->Gmask, Surface->format->Bmask, Surface->format->Amask);
+	
+	int i,j,largeur,hauteur;
+	
+	for(i=0;i<Width;i++)
+	{
+		for(j=0;j<Height;j++)
+		{
+			definirPixel(_ret,i,j,moy_zone(Surface,i*(z_lvl-recouvrement),j*(z_lvl-recouvrement),z_lvl,z_lvl));
+		}
+	}
+	
+	return _ret;
+}
 
+void test_vf(int recouvrement)
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	
+	SDL_Surface* image = SDL_LoadBMP("plan-metro-paris-2017.bmp");
+	SDL_Surface* image_z;
+	SDL_Surface* ecran;
+	
+	SDL_Rect position;position.x=0;position.y=0;
+	
+	int i;
+	
+	for(i=recouvrement+1;i<10;i++)
+	{
+		image_z = dezoom_vf(image,i,recouvrement);
+		ecran = SDL_SetVideoMode(image_z->w,image_z->h,32,SDL_HWSURFACE);
+		
+		SDL_BlitSurface(image_z,NULL,ecran,&position);
+		
+		SDL_Flip(ecran);
+		
+		pause();
+	}
+	
+	SDL_FreeSurface(image);
+	SDL_FreeSurface(ecran);
+	SDL_Quit();
+}
 
 int main()
 {
-	test();
-	
-	
+	//test_v1();
+	//test_v2();
+	test_vf(2);
 	
 	return 0;
 }
