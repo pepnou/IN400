3453b8ace2052dc5cb96885f2bf2d1f32464457e
pepnou <pepnou1@gmail.com>
2017-04-05 11:03:46 +0200

    oui

--- td_os/td5/ex1.c
+++ td_os/td5/ex1.c
@@ -1,24 +1,37 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <fcntl.h>
 
 int main()
 {
+	char xd[] = "mais oui c est clai\' c est la congolexicomatisation des lois du ma\'che";
 	int pipefd[2];
-	if(!pipe(pipefd))exit(EXIT_FAILURE);
+	char buf[1024];
 	
+	if(!pipe(pipefd))exit(EXIT_FAILURE);
 	int pid = fork();
 	
 	if(pid == -1)exit(EXIT_FAILURE);
 	
 	if(pid == 0)
 	{
-		
+		close(pipefd[1]);
+		wait(NULL);
+		read(pipefd[0],buf,1024*sizeof(char));
+		printf("%s\n",buf);
+		close(pipefd[0]);
 	}
 	
 	else
 	{
-		
+		close(pipefd[0]);
+		write(pipefd[1],xd,strlen(xd)*sizeof(char));
+		close(pipefd[1]);
 	}
 	
 	exit(EXIT_SUCCESS);
