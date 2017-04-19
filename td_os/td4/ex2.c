5abb6d13b655faa485f2aa9aae44ec0e1f6bf560
pepnou <pepnou1@gmail.com>
2017-04-19 10:31:27 +0200

    oui

---
+++ td_os/td4/ex2.c
@@ -0,0 +1,52 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <time.h>
+
+typedef struct
+{
+	pthread_mutex_t mutex;
+	pthread_cond_t cond;
+	
+	int remplie;
+	int valeur;
+}mailbox;
+
+void mbox_init(mailbox* mb)
+{
+	mb -> mutex = PTHREAD_MUTEX_INITIALIZER;
+	mb -> cond = PTHREAD_COND_INITIALIZER;
+	mb -> remplie = 0;
+}
+
+void mbox_write(mailbox* mb, int i)
+{
+	pthread_mutex_lock(&(mb -> mutex));
+	
+	if(mb -> remplie != 0) pthread_cond_wait(&(mb -> mutex),&(mb -> cond))
+	
+	pthread_mutex_unlock(&(mb -> mutex));
+}
+
+int mbox_read(mailbox* mb)
+{
+	int res;
+	
+	
+	
+	return res;
+}
+
+int main()
+{
+	
+	
+	
+	exit(0);
+}
+
