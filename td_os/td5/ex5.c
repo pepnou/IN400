ecc42ed4640d9c82b4b52b5458957d6191871d8c
pepnou <pepnou1@gmail.com>
2017-04-26 12:06:29 +0200

    oui

--- td_os/td5/ex5.c
+++ td_os/td5/ex5.c
@@ -10,35 +10,51 @@
 
 #define nbr_thread 20
 
-
-
 int main()
 {
 	srand(time(NULL));
+	char buf[1024];
 	
 	int pipefd[2];
 	if(pipe(pipefd) == -1)exit(EXIT_FAILURE);
 	
-	int i;
+	int i,nbr_fini = 0;
 	for(i=0;i<nbr_thread;i++)
 	{
 		if(fork())break;
 	}
 	
-	
-	
 	if(i == nbr_thread) //pere
 	{
+		close(pipefd[1]);
 		
-		
-		for(i=0;i<nbr_thread;i++) wait();
+		while(nbr_fini<nbr_thread)
+		{
+			
+		}
+		close(pipefd[0]);
 	}
 	else //fils
 	{
+		close(pipefd[0]);
+		
+		if(rand()%100<50) //abonnement
+		{
+			
+		}
+		if(rand()%100<30) //message
+		{
+			
+		}
+		if(rand()%100<20) //liste
+		{
+			
+		}
 		
 		
 		
+		close(pipefd[1]);
 	}
-	
+	exit(0);
 }
 
